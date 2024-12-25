fa011e05c75e013b1457387ba45520ca592bbd20
Eren <erenova6@gmail.com>
2024-12-25 11:45:48 +0300

    upd

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,8 +6,8 @@ function getAdmin(req, res) {
 
 async function deleteUser(req, res) {
   const { deletePostId } = req.body;
-  if (deletePostId.includes === "admin") {
-    await db.deleteUserByIdDB(deletePostId);
+  if (deletePostId.includes("admin")) {
+    await db.deleteUserByIdDB(deletePostId.replace("admin", ""));
     const allUsers = await db.getAllUsersDB();
     console.log(allUsers);
   }
